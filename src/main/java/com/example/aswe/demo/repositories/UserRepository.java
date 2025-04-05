59a15ca8d44170ce245a8c98cb4b78ba4ba7e15f
MariamJan <166922600+MariamJan@users.noreply.github.com>
2025-04-05 18:41:50 +0200

    Update UserRepository.java
    User Repository

--- src/main/java/com/example/aswe/demo/repositories/UserRepository.java
+++ src/main/java/com/example/aswe/demo/repositories/UserRepository.java
@@ -1,7 +1,12 @@
 package com.example.aswe.demo.repositories;
-// import com.example.aswe.demo.models.User;
-// import org.springframework.data.jpa.repository.JpaRepository;
-// extends JpaRepository<User, Long>
-public interface UserRepository  {
-    
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.aswe.demo.models.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    boolean existsByEmail(String email);
+    User findByEmail(String email);
 }
