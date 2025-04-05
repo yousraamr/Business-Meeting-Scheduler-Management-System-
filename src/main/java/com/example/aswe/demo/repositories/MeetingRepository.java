a91b746cbac0caf78c65db3a08defa6029f00bca
MariamJan <166922600+MariamJan@users.noreply.github.com>
2025-04-05 18:41:06 +0200

    Update MeetingRepository.java
    Meeting Repository

--- src/main/java/com/example/aswe/demo/repositories/MeetingRepository.java
+++ src/main/java/com/example/aswe/demo/repositories/MeetingRepository.java
@@ -1,5 +1,20 @@
 package com.example.aswe.demo.repositories;
 
-public interface MeetingRepository {
-    
+import com.example.aswe.demo.models.Meeting;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+@Repository
+public interface MeetingRepository extends JpaRepository<Meeting, Integer> {
+
+    List<Meeting> findByTitleContaining(String keyword);
+    List<Meeting> findByDate(LocalDate date);
+    List<Meeting> findByDateAndTime(LocalDate date, LocalTime time);
+    List<Meeting> findByDateAfter(LocalDate date);
+    List<Meeting> findByTitle(String title);
+
 }
